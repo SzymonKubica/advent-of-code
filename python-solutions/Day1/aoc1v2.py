6134870007ac2792570186377f45830ef4c104aa
bkubica25 <bkubica25@gmail.com>
2023-12-03 19:08:14 +0100

    Finished part 2 of Day 1 in Python

--- python-solutions/Day1/aoc1v2.py
+++ python-solutions/Day1/aoc1v2.py
@@ -1,16 +1,17 @@
 from curses.ascii import isdigit
 
 numbers = {
-    1: 'one'
-    2: 'two'
-    3: 'three'
-    4: 'four'
-    5: 'five'
-    6: 'six'
-    7: 'seven'
-    8: 'eight'
-    9: 'nine'
-}
+    1 : 'one',
+    2 : 'two',
+    3 : 'three',
+    4 : 'four',
+    5 : 'five',
+    6 : 'six',
+    7 : 'seven',
+    8 : 'eight',
+    9 : 'nine',
+    }
+
 
 def getting_input():
     file = open("inpt.txt", "r")
@@ -18,35 +19,49 @@ def getting_input():
     file.close()
     
     return inpt
-# threeightwo > 3ight2 
-# .....eightwo| > ....8wo
-def text_to_digit(inpt: list):
-    for line in inpt:
-        if line.startswith(numbers{i} for i in range(1, 9)):
-            line.replace(range{i}, str(i))
-            
 
-def filter_input(inpt: list):
-    filtered_input = []
-    for line in inpt:
-        filtered_input.append(''.join(filter(lambda x: x.isdigit(), line)))
 
-    return filtered_input
+def first(line: str):
+    first_digit = ''
 
-def get_calibration_values(filtered_input: list):
+    for j in range(len(line)):
+        for i in range(1, 10):
+            if line[0].isdigit():
+                first_digit = line[0]
+                return first_digit
+            elif line.startswith(numbers[i], j) or line.startswith(str(i), j):
+                first_digit = str(i)
+                return first_digit
+
+
+def last(line: str):
+    last_digit = ''
+
+    for j in range(len(line)):
+        for i in range(1, 10):
+            if line[0].isdigit():
+                last_digit = line[0]
+                return last_digit
+            elif line.startswith(''.join(reversed(numbers[i])), j) or line.startswith(str(i), j):
+                last_digit = str(i)
+                return last_digit
+
+
+def get_calibration_values(inpt: list):
     calibration_values = []
-    for line in filtered_input:
-        if len(line) >= 2:
-            calibration_values.append(line[0] + line[len(line) - 1])
-        else:
-            calibration_values.append(line[0] + line[0])
+    first_digit = ''
+    last_digit = ''
+
+    for line in inpt:
+       first_digit = first(line)
+       last_digit = last(''.join(reversed(line)))
+       calibration_values.append(first_digit + last_digit)
 
     return calibration_values
 
 
 if __name__ == '__main__':
     inpt = getting_input()
-    filtered_input = filter_input(inpt)
-    calibration_values = get_calibration_values(filtered_input)
+    calibration_values = get_calibration_values(inpt)
     calibration_values = list(map(int, calibration_values))
     print(sum(calibration_values))
