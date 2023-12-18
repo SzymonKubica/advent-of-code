65bb028cb948a9eadb0ea86bced6678dbe9d9820
Szymon Kubica <szymo.kubica@gmail.com>
2023-12-18 13:33:27 +0100

    Added solutions for day 17.

--- rust-solutions/src/day17.rs
+++ rust-solutions/src/day17.rs
@@ -1,81 +1,172 @@
-use priority_queue::PriorityQueue;
+use std::collections::{BinaryHeap, HashMap};
 
 pub fn part1(input_file: &str) {
     let mut grid = read_grid(input_file);
-    let mut grid_copy = grid.clone();
 
-    print_grid(&grid);
     println!("\n");
 
-    let height = grid.len();
-    let width = grid[0].len();
-    grid[0][0].tentative_distance = 0;
+    // Initial two directions
+    let mut tentative_distances: HashMap<Node, usize> = HashMap::new();
+    let start = Point { x: 0, y: 0 };
+    let end = Point {
+        x: grid[0].len() as i32 - 1,
+        y: grid.len() as i32 - 1,
+    };
+    tentative_distances.insert(
+        Node {
+            point: start.clone(),
+            direction: Direction::Right,
+            direction_count: 0,
+        },
+        0,
+    );
+    tentative_distances.insert(
+        Node {
+            point: start.clone(),
+            direction: Direction::Down,
+            direction_count: 0,
+        },
+        0,
+    );
 
-    let mut queue: PriorityQueue<((usize, usize), usize, Direction), u32> = PriorityQueue::new();
+    let mut possible_directions: BinaryHeap<State> = BinaryHeap::new();
 
-    queue.push(((0, 0), 0, Direction::Right), u32::max_value() - 0);
-    queue.push(((0, 0), 0, Direction::Up), u32::max_value() - 0);
+    possible_directions.push(State {
+        cost: 0,
+        node: Node {
+            point: start.clone(),
+            direction: Direction::Right,
+            direction_count: 0,
+        },
+    });
 
-    'outer: while queue.is_empty() == false {
-        let ((position, same_direction_count, direction), priority) = queue.pop().unwrap();
+    possible_directions.push(State {
+        cost: 0,
+        node: Node {
+            point: start.clone(),
+            direction: Direction::Down,
+            direction_count: 0,
+        },
+    });
 
-        for turn_direction in direction.available_turns() {
-            let new_position = turn_direction.translate_point(position);
-            if new_position.0 >= width as usize || new_position.1 >= height as usize {
+    while let Some(State { cost, node }) = possible_directions.pop() {
+        if node.point == end {
+            println!("Minimum cost to get to the end: {}", cost);
+            break;
+        }
+
+        for neighbour in get_neighbours(node) {
+            if !in_bounds(&neighbour.point, &grid) {
                 continue;
             }
-            if turn_direction == direction && same_direction_count < 3 {
-                let mut next = &mut grid[new_position.1 as usize][new_position.0 as usize];
-                next.previous = position;
-                queue.push(
-                    (new_position, same_direction_count + 1, direction),
-                    priority - next.heat_loss,
-                );
-            } else if turn_direction == direction {
-                continue;
-            } else {
-                let next = &mut grid[new_position.1 as usize][new_position.0 as usize];
-                next.previous = position;
-                queue.push((new_position, 0, turn_direction), priority - next.heat_loss);
+            let new_cost = cost + grid[neighbour.point.y as usize][neighbour.point.x as usize];
+
+            if let Some(old_distance) = tentative_distances.get(&neighbour) {
+                if new_cost >= *old_distance {
+                    continue;
+                }
             }
 
-            if new_position.0 == width as usize - 1 && new_position.1 == height as usize - 1 {
-                let next = &mut grid[new_position.1 as usize][new_position.0 as usize];
-                println!("Found the end!");
-                println!("Total Heat loss: {:?}", u32::max_value() - priority - next.heat_loss);
-                break 'outer;
+            // if we have found a path with lower cost we add it to the possible
+            // directions
+            tentative_distances.insert(neighbour.clone(), new_cost);
+            possible_directions.push(State {
+                cost: new_cost,
+                node: neighbour,
+            });
+        }
+    }
+}
+
+fn get_neighbours_ultra(node: Node) -> Vec<Node> {
+    let mut nodes = vec![];
+    for direction in node.direction.get_available_turns() {
+        if node.direction != direction {
+            if node.direction_count >= 4 {
+                nodes.push(Node {
+                    point: direction.translate_point(&node.point),
+                    direction: direction.clone(),
+                    direction_count: 1,
+                });
             }
+        } else if node.direction_count < 10 {
+            nodes.push(Node {
+                point: direction.translate_point(&node.point),
+                direction: direction.clone(),
+                direction_count: node.direction_count + 1,
+            });
         }
     }
 
-    /*
-    let mut grid_copy = grid.clone();
-    let mut prev = (width - 1, height - 1);
-    while prev != (0, 0) {
-        grid_copy[prev.1][prev.0].visited = true;
-        prev = grid[prev.1][prev.0].previous;
-        print_grid(&grid_copy);
+    nodes
+}
+
+fn get_neighbours(node: Node) -> Vec<Node> {
+    let mut nodes = vec![];
+    for direction in node.direction.get_available_turns() {
+        if node.direction != direction {
+            nodes.push(Node {
+                point: direction.translate_point(&node.point),
+                direction: direction.clone(),
+                direction_count: 1,
+            });
+        } else if node.direction_count < 3 {
+            nodes.push(Node {
+                point: direction.translate_point(&node.point),
+                direction: direction.clone(),
+                direction_count: node.direction_count + 1,
+            });
+        }
     }
 
-    grid_copy[prev.1][prev.0].visited = true;
-    */
+    nodes
 }
 
-#[derive(Clone, Eq, PartialEq, Debug, Hash)]
-struct Node {
-    heat_loss: u32,
-    visited: bool,
-    tentative_distance: u32,
-    previous: (usize, usize),
-    last_steps: Vec<Direction>,
+fn in_bounds(pos: &Point, grid: &Vec<Vec<usize>>) -> bool {
+    0 <= pos.x && pos.x < grid[0].len() as i32 && 0 <= pos.y && pos.y < grid.len() as i32
 }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash, Ord, PartialOrd)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
+struct Node {
+    point: Point,
+    direction: Direction,
+    direction_count: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct State {
+    cost: usize,
+    node: Node,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // We are using a min heap, so we are doing this backwards.
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.node.cmp(&other.node))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 impl Direction {
     fn is_opposite(&self, other: Direction) -> bool {
@@ -87,64 +178,142 @@ impl Direction {
         }
     }
 
-    fn available_turns(&self) -> Vec<Direction> {
+    fn translate_point(&self, point: &Point) -> Point {
         match self {
-            Direction::Up => vec![Direction::Up, Direction::Left, Direction::Right],
-            Direction::Down => vec![Direction::Down, Direction::Left, Direction::Right],
-            Direction::Left => vec![Direction::Up, Direction::Down, Direction::Left],
-            Direction::Right => vec![Direction::Up, Direction::Down, Direction::Right],
+            Direction::Up => Point {
+                x: point.x,
+                y: point.y - 1,
+            },
+            Direction::Down => Point {
+                x: point.x,
+                y: point.y + 1,
+            },
+            Direction::Left => Point {
+                x: point.x - 1,
+                y: point.y,
+            },
+            Direction::Right => Point {
+                x: point.x + 1,
+                y: point.y,
+            },
         }
     }
-    fn translate_point(&self, point: (usize, usize)) -> (usize, usize) {
+
+    fn rotate_clockwise(&self) -> Self {
         match self {
-            Direction::Up => (point.0, if point.1 > 0 { point.1 - 1 } else { 0 }),
-            Direction::Down => (point.0, point.1 + 1),
-            Direction::Left => (if point.0 > 0 { point.0 - 1 } else { 0 }, point.1),
-            Direction::Right => (point.0 + 1, point.1),
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
+    }
+
+    fn rotate_counterclockwise(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+            Direction::Right => Direction::Up,
+        }
+    }
+
+    fn get_available_turns(&self) -> Vec<Direction> {
+        match self {
+            Direction::Up => vec![Direction::Left, Direction::Right, Direction::Up],
+            Direction::Down => vec![Direction::Left, Direction::Right, Direction::Down],
+            Direction::Left => vec![Direction::Left, Direction::Up, Direction::Down],
+            Direction::Right => vec![Direction::Up, Direction::Right, Direction::Down],
         }
     }
 }
 
-impl ToString for Node {
-    fn to_string(&self) -> String {
-        if self.visited {
-            "_".to_string()
-        } else {
-            self.heat_loss.to_string()
-        }
-    }
-}
-
-fn print_grid(grid: &Vec<Vec<Node>>) {
-    for row in grid {
-        println!(
-            "{}",
-            row.iter()
-                .map(|e| e.to_string())
-                .collect::<Vec<String>>()
-                .join("")
-        );
-    }
-}
-
-fn read_grid(input_file: &str) -> Vec<Vec<Node>> {
+fn read_grid(input_file: &str) -> Vec<Vec<usize>> {
     let contents = std::fs::read_to_string(input_file).expect("Unable to read the file.");
     contents
         .trim()
         .split("\n")
         .map(|l| {
             l.chars()
-                .map(|c| c.to_string().parse::<u32>().unwrap())
-                .map(|x| Node {
-                    heat_loss: x,
-                    visited: false,
-                    tentative_distance: u32::max_value(),
-                    previous: (0, 0),
-                    last_steps: vec![],
-                })
+                .map(|c| c.to_string().parse::<usize>().unwrap())
                 .collect()
         })
         .collect()
 }
 
-pub fn part2(input_file: &str) {}
+pub fn part2(input_file: &str) {
+    let mut grid = read_grid(input_file);
+
+    println!("\n");
+
+    // Initial two directions
+    let mut tentative_distances: HashMap<Node, usize> = HashMap::new();
+    let start = Point { x: 0, y: 0 };
+    let end = Point {
+        x: grid[0].len() as i32 - 1,
+        y: grid.len() as i32 - 1,
+    };
+    tentative_distances.insert(
+        Node {
+            point: start.clone(),
+            direction: Direction::Right,
+            direction_count: 0,
+        },
+        0,
+    );
+    tentative_distances.insert(
+        Node {
+            point: start.clone(),
+            direction: Direction::Down,
+            direction_count: 0,
+        },
+        0,
+    );
+
+    let mut possible_directions: BinaryHeap<State> = BinaryHeap::new();
+
+    possible_directions.push(State {
+        cost: 0,
+        node: Node {
+            point: start.clone(),
+            direction: Direction::Right,
+            direction_count: 0,
+        },
+    });
+
+    possible_directions.push(State {
+        cost: 0,
+        node: Node {
+            point: start.clone(),
+            direction: Direction::Down,
+            direction_count: 0,
+        },
+    });
+
+    while let Some(State { cost, node }) = possible_directions.pop() {
+        if node.point == end {
+            println!("Minimum cost to get to the end: {}", cost);
+            break;
+        }
+
+        for neighbour in get_neighbours_ultra(node) {
+            if !in_bounds(&neighbour.point, &grid) {
+                continue;
+            }
+            let new_cost = cost + grid[neighbour.point.y as usize][neighbour.point.x as usize];
+
+            if let Some(old_distance) = tentative_distances.get(&neighbour) {
+                if new_cost >= *old_distance {
+                    continue;
+                }
+            }
+
+            // if we have found a path with lower cost we add it to the possible
+            // directions
+            tentative_distances.insert(neighbour.clone(), new_cost);
+            possible_directions.push(State {
+                cost: new_cost,
+                node: neighbour,
+            });
+        }
+    }
+}
