6b341bb2030c5b6aa4cde3aabd3730562e434333
SzymonKubica <szymo.kubica@gmail.com>
2024-12-07 22:33:26 +0000

    Add solutions for day 4 part1

--- rust-solutions-2024/src/main.rs
+++ rust-solutions-2024/src/main.rs
@@ -8,6 +8,7 @@ extern crate rand;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     let selected_day = std::env::args().nth(1).expect("no day selected");
@@ -18,6 +19,7 @@ fn main() {
         (day1::part1, day1::part2),
         (day2::part1, day2::part2),
         (day3::part1, day3::part2),
+        (day4::part1, day4::part2),
     ];
 
     if let Ok(day) = selected_day.parse::<usize>() {
