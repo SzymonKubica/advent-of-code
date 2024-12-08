c93ad3731a2a4235b1462ec371543782b476be0a
SzymonKubica <szymo.kubica@gmail.com>
2024-12-08 15:56:33 +0000

    Add solution for day 5 part 2

--- rust-solutions-2024/src/day5.rs
+++ rust-solutions-2024/src/day5.rs
@@ -20,11 +20,11 @@ pub fn part1(input_file: &str) {
 }
 
 pub fn part2(input_file: &str) {
-    let (rules, updates) = parse_rules_and_updates(input_file);
+    let (rules, mut updates) = parse_rules_and_updates(input_file);
     println!("Rules:\n{:?}", rules);
     println!("Updates:\n{:?}", updates);
 
-    let incorrect_updates: Vec<&Update> = updates.iter().filter(|u| !is_correct(u, &rules)).collect();
+    let mut incorrect_updates: Vec<&mut Update> = updates.iter_mut().filter(|u| !is_correct(u, &rules)).collect();
 
     println!("Incorrect updates:\n{:?}", incorrect_updates);
     let mut cannot_happen_before_map: HashMap<usize, HashSet<usize>> = HashMap::new();
@@ -35,10 +35,21 @@ pub fn part2(input_file: &str) {
             .insert(r.right_page);
     };
 
-    let mut updates_to_fix: Vec<Vec<Page>> = incorrect_updates.iter()
-        .map(|u| u.0.iter().map(|p| Page::new(*p, &cannot_happen_before_map)).collect()).collect();
+    let mut updates_to_fix: Vec<Vec<Page>> = incorrect_updates.iter_mut()
+        .map(|u| u.0.iter_mut().map(|p| Page::new(*p, &cannot_happen_before_map)).collect()).collect();
 
+    updates_to_fix.iter_mut().for_each(|update| {update.sort()});
 
+    println!("Fixed updates:\n{:?}", &updates_to_fix);
+
+    let middle_elements = updates_to_fix
+        .iter()
+        .map(|u| u[u.len() / 2].number)
+        .collect::<Vec<usize>>();
+    println!("Middle elements of correct updates:\n{:?}", middle_elements);
+
+    let sum: usize = middle_elements.iter().sum();
+    println!("Sum of middle elements: {}", sum);
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -47,14 +58,30 @@ struct Page<'a> {
     ordering_map: &'a HashMap<usize, HashSet<usize>>
 }
 
+impl<'a> Ord for Page<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        return self.partial_cmp(other).unwrap();
+    }
+}
+
 impl<'a> PartialOrd for Page<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        // TODO: figure out how to compare properly before sorting
-        //match self.number.partial_cmp(&other.number) {
-        //    Some(core::cmp::Ordering::Equal) => {}
-        //    ord => return ord,
-        //}
-        //self.ordering_map.partial_cmp(&other.ordering_map)
+        assert!(self.ordering_map == other.ordering_map);
+        if !self.ordering_map.contains_key(&self.number) && !self.ordering_map.contains_key(&other.number) {
+            return Some(core::cmp::Ordering::Equal);
+        };
+
+        if self.ordering_map.contains_key(&self.number) {
+            if self.ordering_map.get(&self.number).unwrap().contains(&other.number) {
+              return Some(core::cmp::Ordering::Less);
+            };
+        };
+        if self.ordering_map.contains_key(&other.number) {
+            if self.ordering_map.get(&other.number).unwrap().contains(&self.number) {
+              return Some(core::cmp::Ordering::Greater);
+            };
+        };
+        Some(core::cmp::Ordering::Equal)
     }
 }
 
