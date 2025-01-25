d73e46e5d12a62149450e075d6aeb9f4cffb2370
SzymonKubica <szymo.kubica@gmail.com>
2025-01-25 20:18:00 +0000

    Add solution for day 6 part 2 in java. Making progress compared to rust

--- 2024/java/src/main/java/solutions/day6/Day6.java
+++ 2024/java/src/main/java/solutions/day6/Day6.java
@@ -1,16 +1,13 @@
 package solutions.day6;
 
-import jdk.jshell.execution.Util;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
 import solutions.CommonStructures;
 import solutions.Solution;
 import solutions.Utils;
 
-import javax.swing.text.html.Option;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -39,8 +36,90 @@ public class Day6 implements Solution {
 
     @Override
     public void secondPart(String inputFile) {
+        final var grid = parseFromInputStream(Utils.readInputAsStream(inputFile));
+        System.out.println(printGrid(grid));
 
+        Guard guard = findGuard(grid).get();
+        var clearGrid = cloneGrid(grid);
+        var clearGuard = new Guard(guard.direction, guard.location);
 
+        Set<CommonStructures.Point> newObstructionPositions = new HashSet<>();
+        do {
+            // We need to do the following:
+            // - Find all possible places for obstacles that won't result in the guard leaving the
+            //   board immediately.
+            // - Once we have found all of those positions, we insert the obstacle one-by-one
+            //   into different copies of the grid and run the search to check if it results in a
+            //   cycle.
+            //final var maybeNewObstruction = searchForObstaclesToTheRight(guard, grid);
+            // Turns out a brute-force trying of all location positions is fast enough.
+            // The whole idea about projecting rays was unnecessary.
+            newObstructionPositions.add(guard.location);
+            //maybeNewObstruction.ifPresent(newObstructionPositions::add);
+            guard.visitCell(grid);
+        } while (guard.takeStepOnGrid(grid).isPresent());
+
+        System.out.printf("New possible obstruction positions: %d\n", newObstructionPositions.size());
+        int cyclesFound = 0;
+        int iterations = 1;
+        for (var position : newObstructionPositions) {
+            System.out.println("Checking obstruction position: %s (%d/%s)".formatted(position, iterations, newObstructionPositions.size()));
+            var newPossibleGrid = cloneGrid(grid);
+            newPossibleGrid.get(position.y()).set(position.x(), GridCell.OBSTACLE_SPECIAL);
+            var newGuard = new Guard(clearGuard.direction, clearGuard.location);
+
+            Set<Pair<CommonStructures.Point, CommonStructures.Direction>> guardStates = new HashSet<>();
+
+            do {
+                if (guardStates.contains(Pair.of(newGuard.location, newGuard.direction))) {
+                    cyclesFound += 1;
+                    break;
+                }
+                newGuard.visitCell(newPossibleGrid);
+                guardStates.add(Pair.of(newGuard.location, newGuard.direction));
+            } while (newGuard.takeStepOnGrid(newPossibleGrid).isPresent());
+
+            iterations += 1;
+        }
+
+        System.out.println("Positions that cause cycles found: %s".formatted(cyclesFound));
+
+    }
+
+    private Optional<CommonStructures.Point> searchForObstaclesToTheRight(Guard guard,
+                                                                          List<List<GridCell>> grid) {
+        // copy the current state of the guard
+        Guard projectionRay = new Guard(guard.direction, guard.location);
+        List<List<GridCell>> projectionGrid = cloneGrid(grid);
+        // save the new location as we only count the new obstruction if it is within the grid.
+        CommonStructures.Point possibleObstructionLocation =
+                projectionRay.location.translateInDirection(
+                guard.direction);
+        if (!possibleObstructionLocation.isInsideGrid(grid)
+            || possibleObstructionLocation.indexGrid(grid) == GridCell.OBSTACLE) {
+            return Optional.empty();
+        }
+
+        projectionRay.turnRight();
+        while (projectionRay.location.isInsideGrid(projectionGrid)) {
+            if (projectionRay.location.indexGrid(projectionGrid) == GridCell.OBSTACLE) {
+                // Obstacle found
+                return Optional.of(possibleObstructionLocation);
+            }
+            projectionRay.visitCell(projectionGrid);
+            projectionRay.location =
+                    projectionRay.location.translateInDirection(projectionRay.direction);
+        }
+
+        return Optional.empty();
+    }
+
+    private static List<List<GridCell>> cloneGrid(List<List<GridCell>> grid) {
+        List<List<GridCell>> projectionGrid = grid.stream()
+                .map(ArrayList::new)
+                .map(l -> (List<GridCell>) l)
+                .toList();
+        return projectionGrid;
     }
 
     private Optional<Guard> findGuard(List<List<GridCell>> grid) {
@@ -63,9 +142,9 @@ public class Day6 implements Solution {
     }
 
     private String printGrid(List<List<GridCell>> grid) {
-        return grid.stream()
+        return "==========\n" + grid.stream()
                 .map(row -> row.stream().map(Objects::toString).collect(Collectors.joining("")))
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining("\n")) + "\n==========\n";
     }
 
     @AllArgsConstructor
@@ -84,7 +163,7 @@ public class Day6 implements Solution {
             if (!nextLocation.isInsideGrid(grid)) {
                 return Optional.empty();
             }
-            while (nextLocation.indexGrid(grid) == GridCell.OBSTACLE) {
+            while (nextLocation.indexGrid(grid) == GridCell.OBSTACLE || nextLocation.indexGrid(grid) == GridCell.OBSTACLE_SPECIAL) {
                 turnRight();
                 nextLocation = location.translateInDirection(direction);
                 if (!nextLocation.isInsideGrid(grid)) {
@@ -93,6 +172,39 @@ public class Day6 implements Solution {
             }
             location = nextLocation;
             return Optional.of(location);
+        }
+
+        /**
+         * For part two we need to keep track of the direction in which the guard was going
+         * when he hit the obstacles. This is needed to correctly find the infinite loops.
+         * We do this by maintaining a map from the position of the guard where he was
+         * located before hitting an obstacle to the direction in which he was going.
+         */
+        public Optional<CommonStructures.Point> takeStepOnGridRecordingObstacleImpactDirections(
+                List<List<GridCell>> grid,
+                Map<CommonStructures.Point, CommonStructures.Direction> obstacleImpactDirections) {
+            CommonStructures.Point nextLocation = location.translateInDirection(direction);
+            if (!nextLocation.isInsideGrid(grid)) {
+                return Optional.empty();
+            }
+            while (nextLocation.indexGrid(grid) == GridCell.OBSTACLE) {
+                obstacleImpactDirections.put(location, direction);
+                turnRight();
+                nextLocation = location.translateInDirection(direction);
+                if (!nextLocation.isInsideGrid(grid)) {
+                    return Optional.empty();
+                }
+            }
+            location = nextLocation;
+            return Optional.of(location);
+        }
+
+        public Optional<GridCell> peekNextLocation(List<List<GridCell>> grid) {
+            CommonStructures.Point nextLocation = location.translateInDirection(direction);
+            if (!nextLocation.isInsideGrid(grid)) {
+                return Optional.empty();
+            }
+            return Optional.of(location.indexGrid(grid));
         }
 
         private void turnRight() {
@@ -112,13 +224,14 @@ public class Day6 implements Solution {
     }
 
     private enum GridCell {
-        EMPTY, GUARD, OBSTACLE, VISITED;
-
+        EMPTY, GUARD, OBSTACLE, OBSTACLE_SPECIAL, VISITED, NEW_OBSTRUCTION_LOCATION; 
         public static GridCell fromChar(char c) {
             return switch (c) {
                 case '.' -> GridCell.EMPTY;
                 case '#' -> GridCell.OBSTACLE;
+                case '$' -> GridCell.OBSTACLE_SPECIAL;
                 case '^' -> GridCell.GUARD;
+                case 'O' -> GridCell.NEW_OBSTRUCTION_LOCATION;
                 default ->
                         throw new IllegalArgumentException(("Unable to parse GridCell from char: "
                                                             + "%s").formatted(c));
@@ -132,7 +245,9 @@ public class Day6 implements Solution {
                 case EMPTY -> ".";
                 case GUARD -> "^";
                 case OBSTACLE -> "#";
+                case OBSTACLE_SPECIAL -> "$";
                 case VISITED -> "X";
+                case NEW_OBSTRUCTION_LOCATION -> "O";
             };
         }
     }
