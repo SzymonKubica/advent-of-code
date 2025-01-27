c3b89f1f07e78bcd97eaa02e3ff9c34deaeff78d
SzymonKubica <szymo.kubica@gmail.com>
2025-01-27 22:28:35 +0000

    Add solution for day 8 part 2 in beautiful java xD

--- 2024/java/src/main/java/solutions/day8/Day8.java
+++ 2024/java/src/main/java/solutions/day8/Day8.java
@@ -4,6 +4,8 @@ import solutions.CommonStructures;
 import solutions.Solution;
 import solutions.Utils;
 
+import javax.swing.text.html.Option;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,13 +32,59 @@ public class Day8 implements Solution {
             uniqueAntinodeLocations.addAll(points);
         }
 
-        uniqueAntinodeLocations = uniqueAntinodeLocations.stream().filter(antinode -> antinode.isInsideGrid(cityMap)).collect(
-                Collectors.toSet());
+        uniqueAntinodeLocations = uniqueAntinodeLocations.stream()
+                .filter(antinode -> antinode.isInsideGrid(cityMap))
+                .collect(Collectors.toSet());
 
-        System.out.printf("There are %d unique antinodes", uniqueAntinodeLocations.size());
+        System.out.printf("There are %d unique antinodes\n", uniqueAntinodeLocations.size());
+
+        markAntinodes(cityMap, uniqueAntinodeLocations);
+        System.out.println(printCityMap(cityMap));
     }
 
-    private Set<CommonStructures.Point> traceAntinodes(List<Antenna> alikeAntennas) {
+    private void markAntinodes(
+            List<List<Optional<Antenna>>> cityMap,
+            Set<CommonStructures.Point> uniqueAntinodeLocations
+    ) {
+        for (final var p : uniqueAntinodeLocations) {
+            cityMap.get(p.y()).set(p.x(), Optional.of(ANTINODE));
+        }
+    }
+
+    @Override
+    public void secondPart(String inputFile) {
+        final var cityMap = getCityMap(Utils.readInputAsStream(inputFile));
+        System.out.println(printCityMap(cityMap));
+
+        // Need to group antennas into sets of same frequency
+
+        List<Antenna> allAntennas = cityMap.stream()
+                .map(row -> row.stream().filter(Optional::isPresent).map(Optional::get).toList())
+                .flatMap(List::stream)
+                .toList();
+
+        Map<Character, List<Antenna>> sameFrequencyAntennaMap = allAntennas.stream()
+                .collect(Collectors.groupingBy(antenna -> antenna.frequency));
+
+        Set<CommonStructures.Point> uniqueAntinodeLocations = new HashSet<>();
+        for (final var alikeAntennas : sameFrequencyAntennaMap.values()) {
+            Set<CommonStructures.Point> points = traceAntinodesPart2(alikeAntennas, cityMap);
+            uniqueAntinodeLocations.addAll(points);
+        }
+
+        uniqueAntinodeLocations = uniqueAntinodeLocations.stream()
+                .filter(antinode -> antinode.isInsideGrid(cityMap))
+                .collect(Collectors.toSet());
+
+        System.out.printf("There are %d unique antinodes\n", uniqueAntinodeLocations.size());
+        markAntinodes(cityMap, uniqueAntinodeLocations);
+        System.out.println(printCityMap(cityMap));
+    }
+
+    private Set<CommonStructures.Point> traceAntinodesPart2(
+            List<Antenna> alikeAntennas,
+            List<List<Optional<Antenna>>> grid
+    ) {
         Set<CommonStructures.Point> antinodes = new HashSet<>();
         for (int i = 0; i < alikeAntennas.size(); i++) {
             for (int j = 0; j < alikeAntennas.size(); j++) {
@@ -44,8 +92,26 @@ public class Day8 implements Solution {
                     CommonStructures.Point a1 = alikeAntennas.get(i).location;
                     CommonStructures.Point a2 = alikeAntennas.get(j).location;
 
-                    CommonStructures.Point antinode1 = a1.translateBy(a2.difference(a1).reflectAboutOrigin());
-                    CommonStructures.Point antinode2 = a2.translateBy(a1.difference(a2).reflectAboutOrigin());
+                    CommonStructures.Point differenceVector = a2.difference(a1);
+                    int u = differenceVector.x();
+                    int v = differenceVector.y();
+
+                    int divisor = gcd(u, v);
+                    CommonStructures.Point diffScaled = new CommonStructures.Point(u / divisor,
+                                                                                   v / divisor);
+                    CommonStructures.Point diffScaledNegative = diffScaled.reflectAboutOrigin();
+
+                    CommonStructures.Point antinode1 = a1;
+                    while (antinode1.isInsideGrid(grid)) {
+                        antinodes.add(antinode1);
+                        antinode1 = antinode1.translateBy(diffScaled);
+                    }
+
+                    CommonStructures.Point antinode2 = a1;
+                    while (antinode2.isInsideGrid(grid)) {
+                        antinodes.add(antinode2);
+                        antinode2 = antinode2.translateBy(diffScaledNegative);
+                    }
 
                     antinodes.add(antinode1);
                     antinodes.add(antinode2);
@@ -56,10 +122,35 @@ public class Day8 implements Solution {
 
     }
 
-    @Override
-    public void secondPart(String inputFile) {
+    private static int gcd(int a, int b) {
+        BigInteger b1 = BigInteger.valueOf(a);
+        BigInteger b2 = BigInteger.valueOf(b);
+        BigInteger gcd = b1.gcd(b2);
+        return gcd.intValue();
+    }
+
+    private Set<CommonStructures.Point> traceAntinodes(List<Antenna> alikeAntennas) {
+        Set<CommonStructures.Point> antinodes = new HashSet<>();
+        for (int i = 0; i < alikeAntennas.size(); i++) {
+            for (int j = 0; j < alikeAntennas.size(); j++) {
+                if (i != j) {
+                    CommonStructures.Point a1 = alikeAntennas.get(i).location;
+                    CommonStructures.Point a2 = alikeAntennas.get(j).location;
+
+                    CommonStructures.Point antinode1 = a1.translateBy(a2.difference(a1)
+                                                                              .reflectAboutOrigin());
+                    CommonStructures.Point antinode2 = a2.translateBy(a1.difference(a2)
+                                                                              .reflectAboutOrigin());
+
+                    antinodes.add(antinode1);
+                    antinodes.add(antinode2);
+                }
+            }
+        }
+        return antinodes;
 
     }
+
 
     private List<List<Optional<Antenna>>> getCityMap(Stream<String> input) {
         List<String> inputCollected = input.toList();
@@ -78,6 +169,7 @@ public class Day8 implements Solution {
 
     // Used only for printing below
     public static final Antenna NO_ANTENNA = new Antenna('.', null);
+    public static final Antenna ANTINODE = new Antenna('#', null);
 
     private String printCityMap(List<List<Optional<Antenna>>> cityMap) {
         return cityMap.stream()
