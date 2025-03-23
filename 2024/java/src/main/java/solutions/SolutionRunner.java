d1705fc83e486e2a4844c955f0f76cdb35b68cc9
SzymonKubica <szymo.kubica@gmail.com>
2025-03-23 16:35:59 +0000

    Add solution for day 22 part 1 that works for example and does not time out for the actual puzzle input. Submission remains

--- 2024/java/src/main/java/solutions/SolutionRunner.java
+++ 2024/java/src/main/java/solutions/SolutionRunner.java
@@ -14,6 +14,7 @@ import solutions.day19.Day19;
 import solutions.day2.Day2;
 import solutions.day20.Day20;
 import solutions.day21.Day21;
+import solutions.day22.Day22;
 import solutions.day3.Day3;
 import solutions.day4.Day4;
 import solutions.day5.Day5;
@@ -53,7 +54,8 @@ public class SolutionRunner {
                                            new Day18(),
                                            new Day19(),
                                            new Day20(),
-                                           new Day21());
+                                           new Day21(),
+                                           new Day22());
 
         int day_idx = day - 1;
         if (day_idx > solutions.size()) {
---
+++ 2024/java/src/main/java/solutions/day22/Day22.java
@@ -0,0 +1,94 @@
+package solutions.day22;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import solutions.Solution;
+import solutions.Utils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Day22 implements Solution {
+    private static final int EVOLUTION_ITERATIONS = 2000;
+
+    @Override
+    public void firstPart(String inputFile) {
+        List<SecretNumber> secretNumbers = readSecretNumbers(Utils.readInputAsStream(inputFile));
+
+
+        int progressLogIterationInterval = 100;
+        for (int j = 0; j < secretNumbers.size(); j++) {
+            SecretNumber number = secretNumbers.get(j);
+            System.out.println("Processing number: %s (%d/%d)".formatted(number.initialValue, j, secretNumbers.size()));
+            for (int i = 0; i < EVOLUTION_ITERATIONS; i++) {
+                number.evolve();
+                if (i % progressLogIterationInterval == 0) {
+                    System.out.println("Progress: (%d/%d)".formatted(i, EVOLUTION_ITERATIONS));
+                }
+            }
+        }
+
+        for (SecretNumber number : secretNumbers) {
+            System.out.println(number.showCurrentState());
+        }
+
+        long secretNumberCurrentValueSum = secretNumbers.stream().map(SecretNumber::getValue)
+                .reduce(Long::sum).get();
+
+        System.out.println("Adding up the 2000th new secret number for each buyer produces %d.".formatted(
+                secretNumberCurrentValueSum));
+
+
+    }
+
+    @Override
+    public void secondPart(String inputFile) {
+
+    }
+
+    private List<SecretNumber> readSecretNumbers(Stream<String> input) {
+        return input.map(SecretNumber::fromString).toList();
+    }
+
+    @ToString
+    @AllArgsConstructor
+    private static class SecretNumber {
+        final long initialValue;
+        @Getter long value;
+
+        static SecretNumber fromString(String input) {
+            long value = Long.parseLong(input);
+            return new SecretNumber(value, value);
+        }
+
+        String showCurrentState() {
+            return "%d: %d".formatted(initialValue, value);
+        }
+
+
+        void evolve() {
+            // First evolution stage
+            long toMix1 = value * 64;
+            this.mix(toMix1);
+            this.prune();
+            // Second evolution stage
+            long toMix2 = value / 32;
+            this.mix(toMix2);
+            this.prune();
+            // Third evolution stage
+            long toMix3 = value * 2048;
+            this.mix(toMix3);
+            this.prune();
+        }
+
+        private void prune() {
+            this.value %= 16777216;
+        }
+
+        private void mix(long mixingCandidate) {
+            this.value ^= mixingCandidate;
+        }
+    }
+}
