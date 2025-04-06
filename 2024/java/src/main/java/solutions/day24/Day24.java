773f45c3ec6f1ac2c40a6bfc539d8d6fc5afdbeb
SzymonKubica <szymo.kubica@gmail.com>
2025-04-06 15:13:00 +0100

    Add solution for day 24 part 1

--- 2024/java/src/main/java/solutions/day24/Day24.java
+++ 2024/java/src/main/java/solutions/day24/Day24.java
@@ -1,11 +1,12 @@
 package solutions.day24;
 
+import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.tuple.Pair;
 import solutions.Solution;
 import solutions.Utils;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Day24 implements Solution {
@@ -13,6 +14,88 @@ public class Day24 implements Solution {
     public void firstPart(String inputFile) {
         Pair<Map<String, Wire>, List<Gate>> wireMapAndGates = processInput(Utils.readInputAsStream(
                 inputFile).collect(Collectors.joining("\n")));
+
+        Map<String, Wire> wireMap = wireMapAndGates.getLeft();
+        List<Gate> gates = wireMapAndGates.getRight();
+
+        // Find all wires that something else depends on
+        Map<Wire, List<Gate>> gatesDependingOnWire = new HashMap<>();
+
+        for (final var gate : gates) {
+            // If the wire is found for the first time, we need to initialize an entry in the map.
+            gatesDependingOnWire.putIfAbsent(gate.left, new ArrayList<>());
+            gatesDependingOnWire.putIfAbsent(gate.right, new ArrayList<>());
+
+            // Mark the current gate as depending on that wire
+            gatesDependingOnWire.get(gate.left).add(gate);
+            gatesDependingOnWire.get(gate.right).add(gate);
+        }
+
+        // Now we need to find the map from wire to the output gate that defines its value
+
+        Map<Wire, Gate> wireValueGateMap = new HashMap<>();
+
+        for (final var gate : gates) {
+            wireValueGateMap.put(gate.output, gate);
+        }
+
+        // Now we get the set of all wires
+        Set<Wire> allWires = new HashSet<>();
+        allWires.addAll(wireMap.values());
+
+        for (final var gate : gates) {
+            allWires.add(gate.left);
+            allWires.add(gate.right);
+            allWires.add(gate.output);
+        }
+
+        // Now we start evaluating from the wires that nothing depends on.
+        List<Wire> wiresToEvaluate = allWires.stream().filter(Predicate.not(gatesDependingOnWire::containsKey)).filter(Wire::isOutput).collect(Collectors.toList());
+
+        while (!wiresToEvaluate.isEmpty()) {
+            Wire current = wiresToEvaluate.removeFirst();
+            // We pick a wire and try to evaluate it, if the inputs are not available, we add them to the queue
+            // and put the wire at the end
+            if (current.value.isPresent()) {
+                continue;
+            }
+
+            Gate gateDefiningWireValue = wireValueGateMap.get(current);
+            System.out.println(gateDefiningWireValue);
+
+            if (gateDefiningWireValue.isReady()) {
+                gateDefiningWireValue.evaluate();
+            } else {
+                Wire left = gateDefiningWireValue.left;
+                Wire right = gateDefiningWireValue.right;
+                if (left.value.isEmpty() && !wiresToEvaluate.contains(left)) {
+                    wiresToEvaluate.add(gateDefiningWireValue.left);
+                }
+                if (right.value.isEmpty() && !wiresToEvaluate.contains(right)) {
+                    wiresToEvaluate.add(gateDefiningWireValue.right);
+                }
+                wiresToEvaluate.add(current);
+            }
+        }
+
+        for (final var wire : allWires) {
+            System.out.println(wire);
+        }
+
+        List<Wire> outputWires = allWires.stream().filter(Wire::isOutput).sorted(Comparator.comparing(wire -> wire.name)).toList();
+
+        System.out.println("Output wires: ");
+        System.out.println(Utils.toStringLineByLine(outputWires));
+
+        long output = processOutputBits(outputWires);
+
+        System.out.println("Output of the circuit: %d".formatted(output));
+
+
+    }
+
+    private long processOutputBits(List<Wire> outputWires) {
+        return outputWires.stream().map(wire -> wire.value.get() ? (long) Math.pow(2, outputWires.indexOf(wire)) : 0).reduce(Long::sum).get();
     }
 
     private Pair<Map<String, Wire>, List<Gate>> processInput(String input) {
@@ -25,7 +108,8 @@ public class Day24 implements Solution {
                 .collect(Collectors.groupingBy(wire -> wire.name)).entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().get(0)));
 
-        List<Gate> gates = wiresAndGates[1].lines().map(line -> parseGate(line, wireNameToWireMap)).toList();
+        List<Gate> gates = wiresAndGates[1].lines().map(line -> parseGate(line, wireNameToWireMap))
+                .toList();
 
         System.out.println(gates);
 
@@ -35,6 +119,14 @@ public class Day24 implements Solution {
     @Override
     public void secondPart(String inputFile) {
 
+        Pair<Map<String, Wire>, List<Gate>> wireMapAndGates = processInput(Utils.readInputAsStream(
+                inputFile).collect(Collectors.joining("\n")));
+
+        Map<String, Wire> wireMap = wireMapAndGates.getLeft();
+        List<Gate> gates = wireMapAndGates.getRight();
+
+        System.out.println(gates.size());
+
     }
 
     private Gate parseGate(String input, Map<String, Wire> wireNameToWireMap) {
@@ -42,28 +134,78 @@ public class Day24 implements Solution {
         String[] leftOperatorRight = inputAndOutput[0].split(" ");
         String output = inputAndOutput[1].trim();
 
-        Wire left = wireNameToWireMap.getOrDefault(leftOperatorRight[0], new Wire(leftOperatorRight[0], false));
-        Wire right = wireNameToWireMap.getOrDefault(leftOperatorRight[2], new Wire(leftOperatorRight[2], false));
-        Wire outputWire = wireNameToWireMap.getOrDefault(output, new Wire(output, false));
+        Wire left = wireNameToWireMap.getOrDefault(
+                leftOperatorRight[0],
+                new Wire(leftOperatorRight[0], Optional.empty())
+        );
+        wireNameToWireMap.put(left.name, left);
+        Wire right = wireNameToWireMap.getOrDefault(
+                leftOperatorRight[2],
+                new Wire(leftOperatorRight[2], Optional.empty())
+        );
+        wireNameToWireMap.put(right.name, right);
+        Wire outputWire = wireNameToWireMap.getOrDefault(output, new Wire(output, Optional.empty()));
+        wireNameToWireMap.put(outputWire.name, outputWire);
 
         Operation operation = Operation.valueOf(leftOperatorRight[1]);
 
         return new Gate(left, right, outputWire, operation);
     }
 
-    record Wire(String name, boolean value) {
+    static final class Wire {
+        private final String name;
+        private Optional<Boolean> value;
+
         @Override
-        public String toString() {
-            return "%s: %d".formatted(name, value ? 1 : 0);
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Wire wire = (Wire) o;
+            return Objects.equals(name, wire.name);
         }
 
-        public static Wire fromString(String input) {
-            String[] parts = input.split(": ");
-            return new Wire(parts[0], parts[1].trim().equals("1"));
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        Wire(String name, Optional<Boolean> value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+            public String toString() {
+                return "%s: %s".formatted(name, value.isPresent() ? (value.get() ? 1 : 0) : "_");
+            }
+
+            public static Wire fromString(String input) {
+                String[] parts = input.split(": ");
+                return new Wire(parts[0], Optional.of(parts[1].trim().equals("1")));
+            }
+
+        public boolean isOutput() {
+            return name.startsWith("z");
         }
     }
 
     record Gate(Wire left, Wire right, Wire output, Operation operation) {
+        public boolean isReady() {
+            return left.value.isPresent() && right.value.isPresent();
+        }
+
+        public void evaluate() {
+            assert isReady();
+
+            boolean left = this.left.value.get();
+            boolean right = this.right.value.get();
+            output.value = Optional.of(switch (operation) {
+                case AND -> left && right;
+                case OR -> left || right;
+                case XOR -> left ^ right;
+            });
+        }
     }
 
     enum Operation {
