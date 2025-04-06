03dd753b6b3859c0c81b7f72eabc3ed133554f17
SzymonKubica <szymo.kubica@gmail.com>
2025-04-06 13:56:47 +0100

    Add solution for day 23 part 2

--- 2024/java/src/main/java/solutions/day23/Day23.java
+++ 2024/java/src/main/java/solutions/day23/Day23.java
@@ -7,6 +7,7 @@ import solutions.Utils;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day23 implements Solution {
     @Override
@@ -45,10 +46,14 @@ public class Day23 implements Solution {
 
         int[][] adjacencyMatrix = new int[connectionMap.size()][connectionMap.size()];
 
-        List<Computer> allComputers = connectionMap.keySet().stream().sorted(Comparator.comparing(computer -> computer.name)).toList();
+        List<Computer> allComputers = connectionMap.keySet().stream()
+                .sorted(Comparator.comparing(computer -> computer.name)).toList();
         System.out.println("There are %s distinct computers".formatted(allComputers.size()));
         Map<Computer, Integer> matrixKeyMap = allComputers.stream()
                 .collect(Collectors.toMap(entry -> entry, allComputers::indexOf));
+
+        Map<Integer, Computer> matrixKeyReverseMap = allComputers.stream()
+                .collect(Collectors.toMap( allComputers::indexOf, entry -> entry));
 
         for (final Computer computer : allComputers) {
             for (final var nb : connectionMap.get(computer)) {
@@ -56,14 +61,90 @@ public class Day23 implements Solution {
             }
         }
 
-        System.out.println(printAdjacencyMatrix(adjacencyMatrix));
+        // The idea is that if we take a subset of n nodes from the graph and count the edges
+        // that are there
+        // if we see that the sum of the edges is  n * (n-1) (each edge is present twice in the
+        // adjacency matrix),
+        // we know that the graph is fully connected
 
+        List<List<Integer>> completeSubGraphs = new ArrayList<>();
+        for (final Computer computer : allComputers) {
+            System.out.println("Processing computer %s. Progress (%d,%d)".formatted(computer, allComputers.indexOf(computer), allComputers.size()));
+            final List<Computer> neighbours = connectionMap.get(computer);
+            // We need to consider all subsets of neighbours plus the current computer
+            // Note that this is bounded by 2^(max set size) which in case of our puzzle input is
+            // 2^14 (should be doable)
+            final List<Computer> subNetwork = Stream.concat(
+                    Stream.of(computer),
+                    neighbours.stream()
+            ).toList();
+            System.out.println("Subnetwork starting from this computer has size %d".formatted(subNetwork.size()));
+            final List<List<Integer>> completeSubsets = getCompleteSubsets(subNetwork.stream().map(matrixKeyMap::get).toList(), adjacencyMatrix, new HashSet<>());
+            completeSubGraphs.addAll(completeSubsets);
+            System.out.println("Found %d complete subsets".formatted(completeSubsets.size()));
+        }
 
+        List<List<Integer>> sortedSubGraphs = completeSubGraphs.stream()
+                .sorted(Comparator.comparingInt(List::size)).toList();
+
+        List<Integer> largestCompleteGraph = sortedSubGraphs.getLast();
+
+        System.out.println(largestCompleteGraph);
+
+        List<Computer> largestCompleteSubNetwork = largestCompleteGraph.stream().map(matrixKeyReverseMap::get).toList();
+
+        String password = largestCompleteSubNetwork.stream().sorted(Comparator.comparing(computer -> computer.name)).map(Computer::name).collect(
+                Collectors.joining(","));
+
+        System.out.println("The password is: %s".formatted(password));
+    }
+
+    private boolean isComplete(
+            List<Integer> subset,
+            int[][] adjacencyMatrix
+    ) {
+        processedSubsets ++;
+        //System.out.println("%d: Checking if %s is complete".formatted(processedSubsets, subset));
+        int n = subset.size();
+        int totalEdges = 0;
+        for (int index : subset) {
+            int[] row = adjacencyMatrix[index];
+            for (int rowIndex : subset) {
+                totalEdges += row[rowIndex];
+            }
+        }
+
+        return totalEdges == n * (n - 1);
+    }
+
+    static int processedSubsets = 0;
+
+    private List<List<Integer>> getCompleteSubsets(List<Integer> subNetwork, int[][] adjacencyMatrix, Set<List<Integer>> alreadyProcessed) {
+        if (alreadyProcessed.contains(subNetwork)) {
+            return List.of();
+        }
+        alreadyProcessed.add(subNetwork);
+        if (subNetwork.size() == 1) {
+            return List.of();
+        }
+        if (isComplete(subNetwork, adjacencyMatrix)) {
+            return List.of(subNetwork);
+        }
+
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < subNetwork.size() - 1; i++) {
+            output.addAll(getCompleteSubsets(Stream.concat(
+                    subNetwork.subList(0, i).stream(),
+                    subNetwork.subList(i + 1, subNetwork.size()).stream()
+            ).toList(), adjacencyMatrix, alreadyProcessed));
+        }
+        return output;
     }
 
     private String printAdjacencyMatrix(int[][] matrix) {
-        return Arrays.stream(matrix).map(row -> Arrays.stream(row).mapToObj(x -> x == 0 ? " " : String.valueOf(x)).collect(Collectors.joining())).collect(
-                Collectors.joining("\n"));
+        return Arrays.stream(matrix)
+                .map(row -> Arrays.stream(row).mapToObj(x -> x == 0 ? " " : String.valueOf(x))
+                        .collect(Collectors.joining())).collect(Collectors.joining("\n"));
     }
 
     private Map<Computer, List<Computer>> getConnectionMap(String inputFile) {
