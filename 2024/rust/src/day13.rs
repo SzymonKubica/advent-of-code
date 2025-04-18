d252766258f7958d7d4495b062eb2c94ab259798
SzymonKubica <szymo.kubica@gmail.com>
2025-04-18 13:06:52 +0200

    Started working on day 13 in rust to regain some speed in rust coding

--- 2024/rust/src/day13.rs
+++ 2024/rust/src/day13.rs
@@ -1,6 +1,14 @@
 use std::fs;
 
 pub fn first_part(input_file: &str) {
+    let input = fs::read_to_string(&input_file).unwrap();
+    println!("{}", input);
+
+    let buttons: Vec<Button> = input.split("\n\n").map(Button::from).collect();
+
+    for button in buttons.iter() {
+        println!("{:?}", button);
+    }
 }
 
 
@@ -8,6 +16,7 @@ pub fn first_part(input_file: &str) {
 
 pub fn second_part(input_file: &str) {}
 
+#[derive(Debug)]
 struct Button(i32, i32);
 
 impl Button {
@@ -16,4 +25,17 @@ impl Button {
     }
 }
 
+
+impl From<&str> for Button {
+    fn from(value: &str) -> Self {
+        let parts: Vec<&str> = value.split(" ").collect();
+        let x_displacement_str = parts.get(2).unwrap();
+        let y_displacement_str = parts.get(3).unwrap();
+
+        let x_displacement = x_displacement_str[2..].parse::<i32>().unwrap();
+        let y_displacement = y_displacement_str[2..].parse::<i32>().unwrap();
+
+        Self::new(x_displacement, y_displacement)
+    }
+}
 
