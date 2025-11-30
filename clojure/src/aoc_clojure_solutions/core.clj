36af8fe084fdc0375ba7f6539ca14b382ccf5cfd
SzymonKubica <szymo.kubica@gmail.com>
2025-11-30 19:37:10 +0000

    Add wiring to allow for selecting the solution implementation from the CLI

--- clojure/src/aoc_clojure_solutions/core.clj
+++ clojure/src/aoc_clojure_solutions/core.clj
@@ -2,9 +2,25 @@
   (:gen-class)
   (:require [aoc-clojure-solutions.year2022.day-1 :as day-1-2022]))
 
+(def solutions-per-year {2022
+                         {1 [day-1-2022/first_part day-1-2022/second_part]}})
+
+(defn get-solution
+  "Performs a lookup in the solutions per year map above to get the required implementation"
+  [year day part] (get (get (get solutions-per-year year) day) (dec part)))
+
 (defn -main
   "Main entrypoint that dispatches into solutions files."
   [& args]
-  (println (day-1-2022/first_part "input-files")))
+  (let [year (Integer/parseInt (first args))
+        day (Integer/parseInt (nth args 1))
+        part (Integer/parseInt (nth args 2))
+        input-file (nth args 3)]
+    (println "Executing Clojure solution for  year: " year ", day: " day ", part: " part ".")
+    (println "Getting input from file: " input-file)
+    (let [solution (get-solution year day part)]
+      (solution input-file))
+    )
+  )
 
-[1 2 3]
+(-main "2022" "1" "2" "../input-files/2022/day-1-puzzle-input")
--- clojure/src/aoc_clojure_solutions/year2022/day_1.clj
+++ clojure/src/aoc_clojure_solutions/year2022/day_1.clj
@@ -1,27 +1,61 @@
 (ns aoc-clojure-solutions.year2022.day-1
   (:require [clojure.string :as str]))
 
-(defn get-calorie-count-vectors
-  [lines vectors current-vector]
+(defn tail [vector] (subvec vector 1))
+
+(defn get-vectors-splitting-on-empty-lines-explicit-recur
+  "Given a list of string lines, each line being either blank or containing a single integer,
+  it returns a vector of vectors of integers, delimited on blank lines.
+  This is a version of the function that uses explicit recursion, this
+  is not actually used by the solution, but I left it here as a trace
+  of my learning process.
+  "
+  [lines vectors curr]
   (if (empty? lines)
-    vectors
-    (if (str/blank? (get lines 0))
-      (get-calorie-count-vectors (subvec lines 1) (conj vectors current-vector) [])
-      (get-calorie-count-vectors (subvec lines 1) vectors (conj current-vector (Integer/parseInt (get lines 0)))))))
+    vectors                                                 ; Accumulation is complete
+    (if (str/blank? (first lines))
+      ; Blank line: need to end the current vector and add it to the accumulated list.
+      (get-vectors-splitting-on-empty-lines-explicit-recur (tail lines) (conj vectors curr) [])
+      ; Line with a number: we parse the number and append it to the current vector
+      (get-vectors-splitting-on-empty-lines-explicit-recur (tail lines) vectors (conj curr (Integer/parseInt (first lines)))))))
 
-(defn first_part [file-name] (println "Supplied file name" file-name)
-  (let [file-lines (-> file-name slurp str/split-lines)
-        calorie-count-vectors (get-calorie-count-vectors file-lines [] [])
-        calorie-sums (map (fn [vector] (reduce + vector)) calorie-count-vectors)
-        calorie-sums-sorted (vec (sort calorie-sums))
-        max-calories (get calorie-sums-sorted (dec (count calorie-sums-sorted)))
+
+(defn get-vectors-splitting-on-empty-lines
+  "Given a list of string lines, each line being either blank or containing a single integer,
+  it returns a vector of vectors of integers, delimited on blank lines."
+  [lines]
+  (->> lines
+       (partition-by str/blank?)
+       (remove #(str/blank? (first %)))
+       (map #(map (fn [number-str] (Integer/parseInt number-str)) %))))
+
+(defn read-file-lines
+  "Reads all lines from a given file."
+  [file-name] (-> file-name slurp str/split-lines))
+
+(defn first_part [file-name]
+  (let [file-lines (read-file-lines file-name)
+        food-carried-by-each-elf (get-vectors-splitting-on-empty-lines file-lines)
+        total-calories-per-elf (map #(reduce + %) food-carried-by-each-elf)
+        max-calories (apply max total-calories-per-elf)
         ]
-    (println calorie-count-vectors)
-    (println calorie-sums)
+    (println food-carried-by-each-elf)
+    (println total-calories-per-elf)
     (println max-calories)
-    calorie-count-vectors))
+    max-calories))
 
+(defn second_part [file-name]
+  (let [file-lines (read-file-lines file-name)
+        food-carried-by-each-elf (get-vectors-splitting-on-empty-lines file-lines)
+        total-calories-per-elf-descending (vec (reverse (sort (map #(reduce + %) food-carried-by-each-elf))))
+        top-three-elves (subvec total-calories-per-elf-descending 0 3)
+        top-three-elves-sum (reduce + top-three-elves)
+        ]
+    (println food-carried-by-each-elf)
+    (println total-calories-per-elf-descending)
+    (println top-three-elves)
+    (println top-three-elves-sum)
+    top-three-elves-sum))
 
 (first_part "../input-files/2022/day-1-puzzle-input")
-
-(System/getProperty "user.dir")
+(second_part "../input-files/2022/day-1-puzzle-input")
